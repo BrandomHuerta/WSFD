bf5220e1621681cc51c00e34891a000687ffe4f8
Jose Luis Hdz <utp0151511@alumno.utpuebla.edu.mx>
2025-06-02 11:30:46 -0600

    Se agragaron las clases EmpleadoPermanente y EmpleadoTemporal

---
+++ EmpleadoPermanente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSFD
+{
+  internal class EmpleadoPermanente : Empleado
+  {
+      public double Bono { get; set; }
+
+    public override double CalcularSueldo()
+    {
+      return SueldoBase + Bono;
+    }
+  }
+}
---
+++ EmpleadoTemporal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSFD
+{
+  internal class EmpleadoTemporal : Empleado
+  {
+    public int HorasExtra { get; set; }
+    public override double CalcularSueldo() 
+    {
+      return SueldoBase + (HorasExtra * 100);
+    }
+  }
+}
